fdd96e0f80e2f59bca874d8151a74897a00394ad
Adeniyi Peter <adeniyipeteroluwatosin@gmail.com>
2024-08-27 10:31:48 +0100

    mobile logo

--- resources/views/layouts/overall.blade.php
+++ resources/views/layouts/overall.blade.php
@@ -49,20 +49,20 @@
 		<div id="kt_header_mobile" class="header-mobile align-items-center header-mobile-fixed">
 			<!--begin::Logo-->
 			<a href="index.html">
-				<img alt="Logo" src="{{ asset('assets/media/logos/whitelogo.png') }}" class="max-h-30px"/>
+				<img alt="Logo" src="{{ asset('assets/media/logos/logo-white.png') }}" class="max-h-30px"/>
 			</a>
 			<!--end::Logo-->
 			<!--begin::Toolbar-->
 			<div class="d-flex align-items-center">
 				<!--begin::Aside Mobile Toggle-->
-				<button class="btn p-0 burger-icon burger-icon-left" id="kt_aside_mobile_toggle">
+				<button class="btn p-0 burger-icon ml-4" id="kt_aside_mobile_toggle">
 					<span></span>
 				</button>
 				<!--end::Aside Mobile Toggle-->
 				<!--begin::Header Menu Mobile Toggle-->
-				<button class="btn p-0 burger-icon ml-4" id="kt_header_mobile_toggle">
+				{{-- <button class="btn p-0 burger-icon ml-4" id="kt_header_mobile_toggle">
 					<span></span>
-				</button>
+				</button> --}}
 				<!--end::Header Menu Mobile Toggle-->
 				<!--begin::Topbar Mobile Toggle-->
 				<button class="btn btn-hover-text-primary p-0 ml-2" id="kt_header_mobile_topbar_toggle">
